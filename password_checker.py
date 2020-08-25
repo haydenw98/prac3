3fa914ab28932db2bdb8217db6de5e380ca397a2
Hayden Watts <hayden.watts98@yahoo.com>
2020-08-25 14:58:35 +1000

    Refactor password check program to use functions

--- password_checker.py
+++ password_checker.py
@@ -1,9 +1,18 @@
 MIN_LENGTH = 6
 
 def main():
+    password = password_get()
+    password_print(password)
+
+
+def password_print(password):
+    print("*" * len(password))
+
+
+def password_get():
     password = input("Enter a password no less than {} characters: ".format(MIN_LENGTH))
     while len(password) < MIN_LENGTH:
         password = input("Enter a password no less than {} characters: ".format(MIN_LENGTH))
-    print("*" * len(password))
+    return password
 
 main()
